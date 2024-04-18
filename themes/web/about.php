c33081edc2a09b2b8a32acf0a9d368d3857c54e2
lucasnvs <lucasli.nev@hotmail.com>
2024-04-18 02:15:14 -0300

    fix: about style

--- themes/web/about.php
+++ themes/web/about.php
@@ -2,7 +2,19 @@
 
 <?php $this->layout("master"); ?>
 
-<section>
-    <h2>Brabos</h2>
-    <p>Nós somos a Siboon SkateShop, uma loja de skate de Porto Alegre, RS. Temos as mais exclusivas peças de DROPS das marcas mais brabas da cena, nosso lema é ...</p>
-</section>
+<div id="container-about">
+    <div>
+        <div class="picture-card">
+            <img src="assets/imgs/picture-loja.png">
+            <p>S I B O O N</p>
+        </div>
+    </div>
+    <div class="about">
+        <h2>NÓS SOMOS SIBOON</h2>
+        <p>Siboon SkateShop, uma loja de skate de Porto Alegre, RS. Temos as mais exclusivas peças de DROPS das marcas mais
+            brabas da cena, nosso lema é BLABLABLABLABLABLABLABLABLABLA.</p>
+        <h2>NOSSO INICIO</h2>
+        <p>Começamos com uma pequena loja no centro de POA, com a galera vendo nossas roupas e toda nossa variedade de peças
+            pros seus carrinhos a gente estourou, até conseguirmos nossa atual loja que é o dobro do tamanho da antiga!</p>
+    </div>
+</div>
