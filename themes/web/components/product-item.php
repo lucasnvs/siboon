629feb49a066b357a9cf1e5306c53ba02730d880
lucasnvs <lucasli.nev@hotmail.com>
2024-06-02 23:04:28 -0300

    Structure: Component Design

---
+++ themes/web/components/product-item.php
@@ -0,0 +1,23 @@
+
+<div class="product-container">
+    <a href="<?= url("produto/".$product["url"]) ?>">
+        <div class="image-container">
+            <img src="assets/imgs/camisa-independent.jpeg">
+        </div>
+        <div class="product-description">
+            <p class="title">CAMISA INDEPENDENT BLACK OVERSIZED - Truck Co.</p>
+            <p class="price">R$ 199,90</p>
+            <p class="price">ou R$180,00 no PIX</p>
+        </div>
+    </a>
+    <div class="sizes">
+        <button class="size">P</button>
+        <button class="size">M</button>
+        <button class="size">G</button>
+        <button class="size">GG</button>
+    </div>
+    <!-- seletor de tamanho -->
+    <!-- se não tiver selecionado aparece --> <span>SELECIONE O TAMANHO</span>
+    <!-- se estiver, aparece o botão -->
+    <button class="btn">ADICIONAR NA SACOLA</button>
+</div>
--- themes/web/home.php
+++ themes/web/home.php
@@ -1,4 +1,4 @@
-<?php $this->layout("master", ['title' => $title]); ?>
+<?php $this->layout("master", ['title' => $title, 'products' => $products]); ?>
 <?php $this->start("specific-script"); ?>
     <script src="assets/js/scripts-home.js" async></script>
 <?php $this->end(); ?>
@@ -16,28 +16,10 @@
     <section class="section-products">
         <h2>EM ALTA</h2>
         <div class="container-grid-section">
-            <div class="product-container">
-                <a href="<?= url("produto/tshirt-diamond-black-piano") ?>">
-                    <div class="image-container">
-                        <img src="assets/imgs/camisa-independent.jpeg">
-                    </div>
-                    <div class="product-description">
-                        <p class="title">CAMISA INDEPENDENT BLACK OVERSIZED - Truck Co.</p>
-                        <p class="price">R$ 199,90</p>
-                        <p class="price">ou R$180,00 no PIX</p>
-                    </div>
-                </a>
-                <div class="sizes">
-                    <button class="size">P</button>
-                    <button class="size">M</button>
-                    <button class="size">G</button>
-                    <button class="size">GG</button>
-                </div>
-                <!-- seletor de tamanho -->
-                <!-- se não tiver selecionado aparece --> <span>SELECIONE O TAMANHO</span>
-                <!-- se estiver, aparece o botão -->
-                <button class="btn">ADICIONAR NA SACOLA</button>
-            </div>
+            <?php foreach($products as $product): ?>
+                <?php $this->insert('components/product-item', ['product' => $product]) ?>
+            <?php endforeach ?>
+
         </div>
     </section>
     <!-- banner -->
