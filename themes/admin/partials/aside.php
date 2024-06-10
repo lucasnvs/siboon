a0b023079a8e58a2426bf46ab1fed912b10527ef
lucasnvs <lucasli.nev@hotmail.com>
2024-06-10 02:19:59 -0300

    Fix: Minor

--- themes/admin/partials/aside.php
+++ themes/admin/partials/aside.php
@@ -4,11 +4,16 @@
 
     <ul id="navigation">
         <li><a>Home</a></li>
-        <li><a>Pedidos / Vendas</a></li>
+        <li><a>Vendas</a></li>
         <li><a>Produtos</a></li>
+        <li><a>Clientes</a></li>
         <li><a>Website</a></li>
-        <li><a>Clientes Cadastrados</a></li>
-        <li><a>Perguntas e Respostas</a></li>
-        <li><a>INFO</a></li>
+        <li><a>FAQ</a></li>
+        <li><a>Institucional</a></li>
     </ul>
+
+    <div id="bottom-options">
+        <button> Configurações </button>
+        <button> <- Sair </button>
+    </div>
 </aside>
