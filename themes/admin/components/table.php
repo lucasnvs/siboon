92ba6ed3070814cf7a1719adecdf79838a5772cc
lucasnvs <lucasli.nev@hotmail.com>
2024-06-11 15:24:29 -0300

    Add: Table Component

---
+++ themes/admin/components/table.php
@@ -0,0 +1,18 @@
+
+<table class="default-table">
+    <thead>
+        <?php foreach ($header as $th): ?>
+            <th> <?= $th ?> </th>
+        <?php endforeach; ?>
+    </thead>
+    <tbody>
+        <?php foreach ($data as $tr): ?>
+            <tr>
+                <td>1</td>
+                <td>Camisa</td>
+                <td>R$ 100,00</td>
+                <td>54</td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
