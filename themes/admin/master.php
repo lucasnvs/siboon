3fa28a21a74ed13bfa3dbdaf86af2818cda41f70
lucasnvs <lucasli.nev@hotmail.com>
2024-06-13 15:58:16 -0300

    Fix: Assets

--- themes/admin/master.php
+++ themes/admin/master.php
@@ -3,12 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="<?= assets('icons/siboon-logo-icon.svg') ?>">
+    <link rel="icon" href="<?= assets(resourcePath: 'icons/siboon-logo-icon.svg') ?>">
     <title> ADMIN | Siboon Skate Shop </title>
     <link rel="stylesheet" href="<?= assets(resourcePath: 'css/global.css') ?>">
     <link rel="stylesheet" href="<?= assets('admin', 'css/layout_admin.css') ?>">
     <?php if ($this->section("specific-style")): ?>
         <?= $this->section("specific-style") ?>
+    <?php endif; ?>
+
+    <?php if ($this->section("specific-script")): ?>
+        <?= $this->section("specific-script"); ?>
     <?php endif; ?>
 </head>
 <body>
