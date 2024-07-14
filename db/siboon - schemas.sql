dde5f5af077ad9b5efb88b922928978ac85740c8
lucasnvs <lucasli.nev@hotmail.com>
2024-07-13 21:31:13 -0300

    Update

--- db/siboon - schemas.sql
+++ db/siboon - schemas.sql
@@ -81,7 +81,8 @@ CREATE TABLE orders_products(
     order_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
-	FOREIGN KEY (order_id) REFERENCES orders(id)
+	FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
 -- ---- --
