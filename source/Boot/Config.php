e6cc0f3bf584788f0bfafb1ab1a836b48324c3fd
lucasnvs <lucasli.nev@hotmail.com>
2024-07-20 15:21:16 -0300

    Add: Data Layer

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -5,3 +5,18 @@ const CONF_DB_PASS = "";
 const CONF_DB_NAME = "siboon_db"; // db-inf-3at
 const CONF_URL_TEST = "http://localhost/siboon";
 const CONF_URL_BASE = "http://localhost/siboon";
+
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => CONF_DB_HOST,
+    "port" => "3306",
+    "dbname" => CONF_DB_NAME,
+    "username" => CONF_DB_USER,
+    "passwd" => CONF_DB_PASS,
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
