f1f354e2dada5834f3230c733a29bcbb581018c1
Lucas <lucasli.nev@hotmail.com>
2024-12-24 23:22:07 -0300

    minor

--- source/Support/Response/Response.php
+++ source/Support/Response/Response.php
@@ -48,7 +48,7 @@ class Response
     {
         self::back([
             "type" => "error",
-            "type_error" => $type_error,
+            "data" => $type_error,
             "message" => $message,
         ], $code);
     }
